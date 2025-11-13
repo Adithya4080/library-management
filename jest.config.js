fc6b74e0e849cd592128d8e6ea0cac6744219ec5
adithya4080 <adithyas712@gmail.com>
2025-11-13 10:55:55 +0530

    first commit

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
+  ],
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    'app/components/**/*.tsx',
+    '!**/*.d.ts',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
